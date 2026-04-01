037df49bfb3cf442517af140b7c0500bc17de881
Miguel Angel <68487934+Mundzuk-Uldin@users.noreply.github.com>
2026-03-31 20:05:32 -0400

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,25 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l = 1
+        r = max(piles)
+        best = r
+        while l <= r:
+            m = (l + r)//2
+            i = self.canEatBananas(piles, m, h, best)
+            if i < 0:
+                l = m +1
+            else:
+                best = m
+                r = m-1
+        return best
+    
+    def canEatBananas(self, piles, k, h, best):
+        count = 0
+        for x in piles:
+            count += math.ceil(x/k)
+        if count <= h and k <= best:
+            self.best = k
+            return 0
+        if count > h:
+            return -1
